94b1469fbc07cf3550f2167065297aae7aa73623
Mike Kruger <mikekruger78@gmail.com>
2024-09-08 15:14:57 +0800

    Refactor RegisterPage component to include password complexity validation and fix error handling

--- yabr/src/pages/RegisterPage.tsx
+++ yabr/src/pages/RegisterPage.tsx
@@ -86,8 +86,8 @@ const RegisterPage = () => {
       
       navigate('/login');
     } catch (error) {
-      //showAlert("An error occurred. Please try again later.\n" + JSON.stringify(error), "error");
-      showAlert("An error occurred. Please try again later.", "error");
+      showAlert("An error occurred. Please try again later.\n" + JSON.stringify(error), "error");
+      // showAlert("An error occurred. Please try again later.", "error");
       setError(error.message);
     } finally {
       setLoading(false);
@@ -138,7 +138,7 @@ const RegisterPage = () => {
                     id="password"
                     value={password}
                     onChange={handlePasswordChange}
-                    placeholder="••••••••"
+                    placeholder=""
                     required
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
@@ -159,7 +159,7 @@ const RegisterPage = () => {
                     id="password2"
                     value={password2}
                     onChange={(e) => setPassword2(e.target.value)}
-                    placeholder="••••••••"
+                    placeholder=""
                     required
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
